f8b422f2af02f9f7db116aecf54e324c903ad56c
Malcolm Barrett <malcolmbarrett@gmail.com>
2020-08-22 21:24:48 -0700

    add sept 2020 materials

--- 14-shiny_reactivity/apps/movies/apps/helpers.R
+++
@@ -1,8 +0,0 @@
-# Function for prettifying axis labels
-prettify_label <- function(x){
-  x %>%
-    str_replace_all("_", " ") %>%
-    str_to_title() %>%
-    str_replace("Imdb", "IMDB") %>%
-    str_replace("Mpaa", "MPAA")
-}
